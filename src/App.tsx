4d1d78028d997eb9c47aca28978fbf26f196dbfb
dream.huang <dream.huang@yijinin.com>
2022-09-01 20:09:21 +0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import Page1 from "./pages/page1";
+import Page2 from "./pages/page2";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/page1" element={<Page1 />}>  </Route>
+        <Route path="/page2" element={<Page2 />}>  </Route>
+       
+      </Routes>
+
+      {/* <Routes>
+        {routes.map(({ path, component: RouteComp, async }) => {
+          return (
+            <Route key={path} path={path}>
+              <RouteComp />
+            </Route>
+          );
+        })}
+      </Routes> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/page2.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const Page2: FC = () => {
+  return (
+    <div>
+      <h1>page2</h1>
+      <a href="/page1">去page1</a>
+    </div>
+  );
+};
+
+export default Page2;
