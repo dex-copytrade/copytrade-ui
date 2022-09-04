9f63e3ed066a4e3a9263a122b158595d44d6869c
dream.huang <dream.huang@yijinin.com>
2022-09-04 20:15:26 +0800

    feat: add

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,26 @@ import { useState } from "react";
 import Page1 from "./pages/page1";
 import Page2 from "./pages/page2";
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
+import { ConfigProvider } from "react-vant";
+
+const themeVars = {
+};
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <Routes>
-        <Route path="/page1" element={<Page1 />}>  </Route>
-        <Route path="/page2" element={<Page2 />}>  </Route>
-       
-      </Routes>
-
-      {/* <Routes>
-        {routes.map(({ path, component: RouteComp, async }) => {
-          return (
-            <Route key={path} path={path}>
-              <RouteComp />
-            </Route>
-          );
-        })}
-      </Routes> */}
+      <ConfigProvider themeVars={themeVars}>
+        <Routes>
+          <Route path="/page1" element={<Page1 />}>
+          </Route>
+          <Route path="/page2" element={<Page2 />}>
+          </Route>
+        </Routes>
+      </ConfigProvider>
     </div>
   );
 }
 
 export default App;
+
--- src/pages/page1.tsx
+++ src/pages/page1.tsx
@@ -1,9 +1,16 @@
 import { FC } from "react";
+import { Button } from 'react-vant';
 
 const Page1: FC = () => {
   return (
     <div>
       <h1>page1</h1>
+      <Button > dd</Button>
+      <Button type='primary'>Primary</Button>
+      <Button type='info'>Info</Button>
+      <Button type='default'>Default</Button>
+      <Button type='warning'>Warning</Button>
+      <Button type='danger'>Dangeer</Button>
       <a href="/page2">去page2</a>
     </div>
   );
