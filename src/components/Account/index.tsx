ed42316620ed45e464c823aeb9e39d42aac016d3
crazy.wang <crazy.wang@yijinin.com>
2022-09-08 22:48:10 +0800

    feat: 订阅

--- src/components/Account/index.tsx
+++ src/components/Account/index.tsx
@@ -1,16 +1,22 @@
 import { FC, useState } from "react";
+import SubModal from '../Modal'
 import './index.less'
 
 export const CardItem: FC = () => {
- 
+  
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleCancel = () => {
+    setIsModalOpen(false)
+  }
   return (
+    <>
     <div className="br8 asset-wrap pad24">
       <div className="df">
         <div className="f1 df aic">
           <div className="fw fs18 mr16">CQ3232432...323</div>
           <div className="c999">累计收益总排行&nbsp;<span className="fw c-black">No.03</span></div>
         </div>
-        <div className="btn line2 mr16">订阅</div>
+        <div className="btn line2 mr16" onClick={()=>setIsModalOpen(true)}>订阅</div>
         <div className="btn primary">跟单</div>
       </div>
       <div className="g-df df list">
@@ -19,6 +25,11 @@ export const CardItem: FC = () => {
         <div>当前跟单人数<p>903</p></div>
       </div>
     </div>
+    <SubModal 
+      isOpen={isModalOpen} 
+      account='CQ3232432...323'
+      handleCancel={handleCancel}></SubModal>
+    </>
   )
 };
 
